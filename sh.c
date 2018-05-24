9a2c6e76189c5072e3c06a4b1b9b26f74da24c70
szk3y <d4tt423@gmail.com>
2018-05-24 17:07:40 +0900

    Fix usage

--- sh.c
+++ sh.c
@@ -190,7 +190,7 @@ int main(int argc, char** argv)
 
   if(argc <= 1) {
     fputs("This function is not implemented.\n", stderr);
-    fprintf(stderr, "Usage: %s '<write program here>'\n", argv[0]);
+    fprintf(stderr, "Usage: %s <file>\n", argv[0]);
     exit(1);
   }
 
