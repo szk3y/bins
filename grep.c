935b3aec5123bb338d89a25e412e0b488fde4687
szk3y <d4tt423@gmail.com>
2018-05-26 23:30:20 +0900

    Add -e and -i option to grep

--- grep.c
+++ grep.c
@@ -5,6 +5,8 @@
 
 #define ERR_BUF_SIZE  0x1000
 #define GREP_BUF_SIZE 0x1000
+#define PASS_FILTER (int)(-1)
+#define CUT_FILTER 0
 
 void do_grep(regex_t* pattern, FILE* fp)
 {
@@ -18,26 +20,51 @@ void do_grep(regex_t* pattern, FILE* fp)
 
 int main(int argc, char** argv)
 {
-  int err;
-  char errbuf[ERR_BUF_SIZE];
-  regex_t pattern;
   FILE* fp;
+  char errbuf[ERR_BUF_SIZE];
+  const char* patstr = NULL;
+  int err;
+  int opt;
+  int is_case_insensitive = CUT_FILTER;
+  regex_t pattern;
 
-  if(argc <= 1) {
+  while((opt = getopt(argc, argv, "ie:")) != -1) {
+    switch(opt) {
+      case 'i':
+        is_case_insensitive = PASS_FILTER;
+        break;
+      case 'e':
+        patstr = optarg;
+        break;
+      case '?':
+        fputs("Invalid option found.\n", stderr);
+        exit(1);
+      default:
+        fputs("Option parser: unreachable code.\n", stderr);
+        exit(1);
+    }
+  }
+
+  if(argv[optind] == NULL && patstr == NULL) {
     fprintf(stderr, "Usage: %s <pattern> [files...]\n", argv[0]);
     exit(1);
   }
+  if(patstr == NULL) {
+    patstr = argv[optind];
+    optind++;
+  }
 
-  err = regcomp(&pattern, argv[1], REG_EXTENDED | REG_NOSUB | REG_NEWLINE);
+  err = regcomp(&pattern, patstr,
+      REG_EXTENDED | REG_NOSUB | REG_NEWLINE | (REG_ICASE & is_case_insensitive));
   if(err != 0) {
     regerror(err, &pattern, errbuf, sizeof(errbuf));
     fputs(errbuf, stderr);
     exit(1);
   }
-  if(argc == 2) {
+  if(argc == optind) {
     do_grep(&pattern, stdin);
   } else {
-    for(int i = 2; i < argc; i++) {
+    for(int i = optind; i < argc; i++) {
       fp = fopen(argv[i], "rb");
       if(!fp) {
         perror("fopen");
